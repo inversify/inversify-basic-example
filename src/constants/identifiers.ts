3cd03d094f7e8ccf5b5a4ac8769126a8c8e529d4
Bryan <bwheel181@gmail.com>
2017-12-17 21:55:25 +0000

    Replaces occurrences of Symbol('string') with Symbol.for('string')

--- src/constants/identifiers.ts
+++ src/constants/identifiers.ts
@@ -1,7 +1,7 @@
 const SERVICE_IDENTIFIER = {
-    BATTLE: Symbol("Batle"),
-    WARRIOR: Symbol("Warrior"),
-    WEAPON: Symbol("Weapon")
+    BATTLE: Symbol.for("Batle"),
+    WARRIOR: Symbol.for("Warrior"),
+    WEAPON: Symbol.for("Weapon")
 };
 
 export default SERVICE_IDENTIFIER;
