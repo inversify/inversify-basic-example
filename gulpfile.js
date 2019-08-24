1bb3345c14e02486bd9180e98ad2aaf77e2a48f6
Dan <djcavanagh@gmail.com>
2019-08-23 20:42:35 -0400

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,20 @@
 //* DEPENDENCIES
 //******************************************************************************
 
-var gulp        = require("gulp"),
-    tslint      = require("gulp-tslint"),
-    tsc         = require("gulp-typescript"),
-    runSequence = require("run-sequence");
+var gulp = require("gulp"),
+    tslint = require("gulp-tslint"),
+    tsc = require("gulp-typescript");
 
 //******************************************************************************
 //* LINT
 //******************************************************************************
-gulp.task("lint", function() {
-    var config =  { formatter: "verbose" };
+gulp.task("lint", function () {
+    var config = { formatter: "verbose" };
     return gulp.src([
         "src/**/**.ts"
     ])
-    .pipe(tslint(config))
-    .pipe(tslint.report());
+        .pipe(tslint(config))
+        .pipe(tslint.report());
 });
 
 //******************************************************************************
@@ -26,20 +25,21 @@ gulp.task("lint", function() {
 //******************************************************************************
 var tstProject = tsc.createProject("tsconfig.json", { typescript: require("typescript") });
 
-gulp.task("build", function() {
+gulp.task("build", function () {
     return gulp.src([
         "src/**/*.ts"
     ])
-    .pipe(tstProject())
-    .on("error", function (err) {
-        process.exit(1);
-    })
-    .js.pipe(gulp.dest("dist/"));
+        .pipe(tstProject())
+        .on("error", function (err) {
+            process.exit(1);
+        })
+        .js.pipe(gulp.dest("dist/"));
 });
 
 //******************************************************************************
 //* DEFAULT
 //******************************************************************************
-gulp.task("default", function (cb) {
-  runSequence("lint", "build", cb);
-});
+gulp.task("default", gulp.series(
+    "lint",
+    "build",
+));
